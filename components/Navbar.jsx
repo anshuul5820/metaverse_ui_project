e2960c48ee1be282b2c31a3e4e331e7e2e36c285
Ansul Sah <anshulsah505@gmail.com>
2023-02-11 20:11:45 +0530

    first commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { motion } from "framer-motion";
+import styles from "../styles";
+import { navVariants } from "../utils/motion";
+
+const Navbar = () => (
+  <motion.nav
+    variants={navVariants}
+    initial="hidden"
+    whileInView="show"
+    className={`${styles.xPaddings} py-8 relative`}
+  >
+    navbar
+  </motion.nav>
+);
+
+export default Navbar;
